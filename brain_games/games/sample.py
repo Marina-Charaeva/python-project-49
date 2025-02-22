9025e18afcaf91a1ce1dea00d82de22f5cedb813
Marina Charaeva <panivan1298@mail.ru>
2025-02-22 15:43:48 +0000

    Fixed errors in brain_even

--- brain_games/games/sample.py
+++ brain_games/games/sample.py
@@ -4,7 +4,7 @@ import prompt
 def start_game(game_name):
     #  Приветствие пользователя
     print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name? ')
+    name = prompt.string("May I have your name? ")
     print(f'Hello, {name}!')
     print(game_name.TASK)
     for i in range(3):

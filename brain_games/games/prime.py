c33f14b0ba85a20ce52477b92cf11d3921626fea
Marina Charaeva <panivan1298@mail.ru>
2025-02-22 15:48:35 +0000

    Fixed errors in brain_evenFixed errors in brain_even

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,6 +1,6 @@
 import random
 
-TASK = 'Answer 'yes' if given number is prime. Otherwise answer 'no'.'
+TASK = 'Answer \'yes\' if given number is prime. Otherwise answer \'no\'.'
 
 
 def logic_game():
